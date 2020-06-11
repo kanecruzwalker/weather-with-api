03cd16abac0e584754a9f48adfeecb57351aeb22
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-10 17:11:14 -0700

    using .write method to write texxxt and then send using .send() all in backend server.

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const https = require("https");
 
 app.get("/", function (req, res){
     
-
-    //  add vairable for api query
-    // use it in this get route
-    // var apiKey = "daf08b427b26f11b51a8b3849391acfc";
     const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=daf08b427b26f11b51a8b3849391acfc&units=imperial";
     https.get(queryUrl, function (response){
         console.log(response.data);
@@ -18,17 +14,19 @@ app.get("/", function (req, res){
         response.on("data", function (data){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
-            console.log(temp);
+
             const description = weatherData.weather[0].description;
-            console.log(description);
+
+ 
+
+            // now time to respond from our server
+            res.write("<h1>The temperature is " + temp + "</h1>");
+            res.write("<h2>The current description is: " + description+"</h2>");
+            res.send();
         });
-
     });
-
-    
-    res.sendFile(__dirname + "/index.html");
-})
+});
 
 app.listen(3000, function (){
     console.log("Server Listening on port 3000");
-})
+});
